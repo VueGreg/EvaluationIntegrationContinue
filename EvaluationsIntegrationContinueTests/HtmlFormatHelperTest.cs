71fcc73ecf632723c9c6661b0016aef6a429cc7e
Grégory WOLFF <gregory.wolff.dev@gmail.com>
2025-01-16 16:39:06 +0100

    Feat : Add HtmlFormatHelper test

---
+++ EvaluationsIntegrationContinueTests/HtmlFormatHelperTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EvaluationIntegrationContinue;
+
+namespace EvaluationsIntegrationContinueTests
+{
+    [TestClass]
+    public class HtmlFormatHelperTest
+    {
+        // Initialisation des méthodes à tester
+        private HtmlFormatHelper _htmlFormatHelper;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _htmlFormatHelper = new HtmlFormatHelper();
+        }
+
+        [TestMethod]
+        public void GetStrongFormat_WithPersonnalText_Test()
+        {
+            var content = "C'est un peu gras içi !";
+
+            var result = _htmlFormatHelper.GetStrongFormat(content);
+
+            Assert.AreEqual("<strong>C'est un peu gras içi !</strong>", result);
+        }
+
+        [TestMethod]
+        public void GetItalicFormat_WithPersonnalText_Test()
+        {
+            var content = "C'est un peu penché içi !";
+
+            var result = _htmlFormatHelper.GetItalicFormat(content);
+
+            Assert.AreEqual("<i>C'est un peu penché içi !</i>", result);
+        }
+
+        [TestMethod]
+        public void GetFormattedListElements_WhenListContainsThreeItems_Test()
+        {
+            var contents = new List<string> { "Item 1", "Item 2", "Item 3" };
+
+            var result = _htmlFormatHelper.GetFormattedListElements(contents);
+
+            var expected = "<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>";
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
