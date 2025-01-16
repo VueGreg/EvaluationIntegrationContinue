1a326c0dc699e72df986f83b47226c7123bed61f
Grégory WOLFF <gregory.wolff.dev@gmail.com>
2025-01-16 16:24:20 +0100

    Feat : Add tests to Baptizm

---
+++ EvaluationsIntegrationContinueTests/BaptismTests.cs
@@ -0,0 +1,67 @@
+﻿using EvaluationIntegrationContinue;
+using Moq;
+
+namespace EvaluationsIntegrationContinueTests
+{
+
+    [TestClass]
+    public class BaptismTests
+    {
+        private ClergyMember _clergyMember;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _clergyMember = new ClergyMember();
+        }
+
+        [TestMethod]
+        public void CanBeBaptizedByPriest_Test()
+        {
+            _clergyMember.IsPriest = true;
+
+            var baptizm = new Baptizm(_clergyMember);
+
+            var result = baptizm.CanBeBaptizedBy(_clergyMember);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeBaptizedByPope_Test()
+        {
+            _clergyMember.IsPope = true;
+
+            var baptizm = new Baptizm(_clergyMember);
+
+            var result = baptizm.CanBeBaptizedBy(_clergyMember);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeTeachedByPope_Test()
+        {
+            _clergyMember.IsPope = true;
+
+            var baptizm = new Baptizm(_clergyMember);
+
+            var result = baptizm.CanBeTeachedBy(_clergyMember);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeTeachedByPriest_Test()
+        {
+            _clergyMember.IsPriest = true;
+
+            var baptizm = new Baptizm(_clergyMember);
+
+            var result = baptizm.CanBeTeachedBy(_clergyMember);
+
+            Assert.IsTrue(result);
+        }
+    }
+
+}
