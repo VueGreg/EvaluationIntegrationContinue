9bad41b4da96121287e1d1aa56d4378bfeb8a3fc
Grégory WOLFF <gregory.wolff.dev@gmail.com>
2025-01-16 15:57:39 +0100

    Feat : First commit with import class

---
+++ EvaluationIntegrationContinue/MathOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationIntegrationContinue
+{
+    public class MathOperations
+    {
+        public int Subtract(int numberOne, int numberTwo)
+        {
+            if (numberOne < numberTwo)
+                throw new ArgumentException("Second parameter cannot be higher than first parameter");
+
+            if (numberOne >= 1200)
+                throw new ArgumentException("First parameter cannot be higher or equal to 1200");
+
+            return numberOne - numberTwo;
+        }
+
+        public string GetColorFromOddsNumber(int number)
+        {
+            if (number < 0)
+                throw new ArgumentException("Limit argument can't be negative");
+
+            if (number % 2 == 0)
+                return "Red";
+
+            return "Blue";
+        }
+    }
+}
